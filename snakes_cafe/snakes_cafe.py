2d24b91ec9d53e662a15ffa2edc733cb4fad35d8
Adam Owada <adamowada@gmail.com>
2020-05-30 23:06:18 -0700

    added attribution

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,3 +1,4 @@
+# https://www.techbeamers.com/python-multiline-string/ helped me with multiline strings
 menu = """**************************************
 **    Welcome to the Snakes Cafe!   **
 **    Please see our menu below.    **
@@ -40,13 +41,18 @@ meal = {}
 
 order = input()
 
+# https://perso.limsi.fr/pointal/_media/python:cours:mementopython3-english.pdf helped me with while loops
 while order != "quit":
-  if order in meal:
+  #  https://www.geeksforgeeks.org/python-check-whether-given-key-already-exists-in-a-dictionary/ helped me with if ... in ... logic
+  if order in meal: 
+    #  https://perso.limsi.fr/pointal/_media/python:cours:mementopython3-english.pdf helped me with dictionaries
     meal.update({order: meal[order] + 1})
     print("\n**" + str(meal[order]) + " orders of " + order + " have been added to your meal**\n")
+  #  https://perso.limsi.fr/pointal/_media/python:cours:mementopython3-english.pdf helped me with if else statements
   else:
     meal.update({order: 1})
     print("\n**" + str(meal[order]) + " order of " + order + " have been added to your meal**\n")
   order = input()
 
+# https://www.geeksforgeeks.org/python-exit-commands-quit-exit-sys-exit-and-os-_exit/ helped me with exit command
 quit()
